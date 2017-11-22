22c607e9ab766e55a40f4c919470f04d9185b924
Ed Overton <e.overton@gmail.com>
2017-11-22 15:36:48 -0500

    db2 config

--- recipes/install_agent-db2.rb
+++ recipes/install_agent-db2.rb
@@ -1,14 +1,26 @@
-node.default['apm_agent']['silent_file'] = 'APM_silent_install-db2.txt'
-node.default['apm_agent']['agent_bin']   = "#{node['apm_agent']['apm_dir']}/bin/db2-agent.sh"
-node.default['apm_agent']['log_file']    = 'agent_install-db2.log'
+agent = 'db2'
+node.default['apm_agent']['silent_file'] = "APM_silent_install-#{agent}.txt"
+node.default['apm_agent']['agent_bin']   = "#{node['apm_agent']['apm_dir']}/bin/#{agent}-agent.sh"
+node.default['apm_agent']['log_file']    = "agent_install-#{agent}.log"
+node.default['apm_agent']['silent_config'] = "APM_silent_config-#{agent}.txt"
+node.default['apm_agent']['config_log'] = "agent_config-#{agent}.log"
 
 include_recipe '::install_agent'
 
+# Write silent contents to a file
+template "#{node['temp_dir']}/#{node['apm_agent']['silent_config']}" do
+  source "#{node['apm_agent']['silent_config']}.erb"
+  action :create
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
 # configure db2 agent using silent file, write output to log
-execute 'configure_db2' do
+execute "configure_#{agent}" do
   command "#{node['apm_agent']['agent_bin']} config db2apm \
-#{node['apm_agent']['apm_dir']}/samples/DB2_silent_config.txt > \
-#{node['temp_dir']}/#{node['apm_agent']['log_file']} 2>&1"
+#{node['temp_dir']}/#{node['apm_agent']['silent_config']} > \
+#{node['temp_dir']}/#{node['apm_agent']['config_log']} 2>&1"
   cwd node['apm_agent']['apm_dir']
   # not_if { File.exist?(node['apm_agent']['agent_bin']) }
   user 'root'
