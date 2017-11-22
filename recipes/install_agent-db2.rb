2e2eefe816928f58772612e7ce752803d577120c
Ed Overton <e.overton@gmail.com>
2017-11-21 20:58:40 -0500

    add different logs

--- recipes/install_agent-db2.rb
+++ recipes/install_agent-db2.rb
@@ -1,3 +1,4 @@
 node.default['apm_agent']['silent_file'] = 'APM_silent_install-db2.txt'
 node.default['apm_agent']['agent_bin']   = "#{node['apm_agent']['apm_dir']}/bin/db2-agent.sh"
+node.default['apm_agent']['log_file']    = 'agent_install-db2.log'
 include_recipe '::install_agent'
--- recipes/install_agent-was.rb
+++ recipes/install_agent-was.rb
@@ -1,2 +1,4 @@
 node.default['apm_agent']['silent_file'] = 'APM_silent_install-was.txt'
+node.default['apm_agent']['agent_bin']   = "#{node['apm_agent']['apm_dir']}/bin/was-agent.sh"
+node.default['apm_agent']['log_file']    = 'agent_install-was.log'
 include_recipe '::install_agent'
