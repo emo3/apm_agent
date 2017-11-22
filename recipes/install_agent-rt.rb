5b4abadab6f2053e07a2d2d32af8a644875bea8f
Ed Overton <e.overton@gmail.com>
2017-11-21 20:51:52 -0500

    fix rt-1

--- recipes/install_agent-rt.rb
+++ recipes/install_agent-rt.rb
@@ -1,3 +1,4 @@
 node.default['apm_agent']['silent_file'] = 'APM_silent_install-rt.txt'
 node.default['apm_agent']['agent_bin']   = "#{node['apm_agent']['apm_dir']}/bin/rt-agent.sh"
+node.default['apm_agent']['log_file']    = 'agent_install-rt.log'
 include_recipe '::install_agent'
