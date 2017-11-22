d6933f98a992bb36efe77ba6c77bf26c23988e38
Ed Overton <e.overton@gmail.com>
2017-11-21 19:52:02 -0500

    add other agents2

---
+++ recipes/install_agent-db2.rb
@@ -0,0 +1,2 @@
+node.default['apm_agent']['silent_file'] = 'APM_silent_install-db2.txt'
+include_recipe '::install_agent'
---
+++ recipes/install_agent-was.rb
@@ -0,0 +1,2 @@
+node.default['apm_agent']['silent_file'] = 'APM_silent_install-was.txt'
+include_recipe '::install_agent'
